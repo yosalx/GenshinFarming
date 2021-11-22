c69367be4880ad780592723ad40f77c56f22b36d
Azmi Alfatih S <73034464+alfatihs@users.noreply.github.com>
2021-11-22 15:31:51 +0700

    start dan beberapa kendali dasar

---
+++ start.pl
@@ -0,0 +1,58 @@
+:- dynamic(game_start/1).
+
+game_start(false).
+
+start :-
+    write('~ Harvest Star ~'), nl, nl, 
+    write('1. start     : untuk memulai petualanganmu'), nl,
+    write('2. map       : menampilkan peta'), nl,
+    write('3. status    : menampilkan kondisimu terkini'), nl,
+    write('4. w         : gerak ke utara 1 langkah'), nl,
+    write('5. s         : gerak ke selatan 1 langkah'),nl,
+    write('6. d         : gerak ke timur 1 langkah'),nl,
+    write('7. a         : gerak ke barat 1 langkah'), nl,
+    write('8. help      : menampilkan segala bantuan'), nl,
+    write('9. Status    : menampilkan status pemain'), nl,nl,nl,nl,
+
+
+    
+    pilihClass.
+
+
+pilihClass :-
+    repeat,
+    write('Welcome to Harvest Star. Choose your job\n1. Fisherman\n2. Farmer\n3. Rancher'), nl,
+    read(X),
+    pickClass(X),!,
+    createChara(X)
+    .
+
+quit:-
+    write('Apakah Anda yakin untuk keluar dari permainan? (y/n)'),
+    read(Order),
+    (Order = y -> halt; (Order = n -> fail)).
+
+help:-
+    write('############################ Command ############################'),nl,
+    write('help  : menampilkan segala bantuan'),nl,
+    write('start : memulai petualangan dari awal'), nl,
+    write('status: menampilkan kondisi pemain saat ini'), nl,
+    write('quit  : mengakhiri petualangan'), nl, nl, nl,
+
+    write('############################ Navigasi ############################'), nl,
+    write('map      : melihat map'),nl,
+    write('w,a,s,d  : untuk bergerak, pemain tidak dapat bergerak ke danau'), nl.
+    
+
+inventory:-
+    write('Inventory:'),nl,
+    write(' Saat ini inventory-mu masih kosong.'),nl.
+    
+
+
+
+
+ 
+ 
+
+
