f9b6a47b8824df0c4730aa4fbd138243e1a24419
yosalx <yoseph141002@gmail.com>
2021-11-22 13:03:09 +0700

    fix typo

--- inventory.pl
+++ inventory.pl
@@ -1,12 +1,12 @@
 inventory :-
-    totalInventory(sum),
-    sum is 0,
-    write('Inventory sementara kosong')
+    totalInventory(Sum),
+    Sum is 0,
+    write('Inventory sementara kosong').
 
 inventory :-
-    totalInventory(sum),
-    sum > 0,
+    totalInventory(Sum),
+    Sum > 0,
     write('Your inventory ('), write(sum), write(' /100)'), nl.
 
-:- dynamic(totalInventory/1)
+:- dynamic(totalInventory/1).
 totalInventory(0).
