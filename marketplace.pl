840728a6d7afe9dfe17ec3b72bc482ed83d8a156
yosalx <yoseph141002@gmail.com>
2021-11-22 14:05:00 +0700

    update marketplace

--- marketplace.pl
+++ marketplace.pl
@@ -1,26 +1,37 @@
-gettoMarket :-  write('Welcome to the shop adventurer!'), nl,
-                write('what can i help you with?'),nl,
+gettoMarket :-  
+                write('|--------------------------------------------------|'),nl,
+                write('|          Welcome To The Marketplace !            |'),nl,
+                write('|            What do you want to do ?              |')
+                write('|--------------------------------------------------|'),nl,
+                write('|         1. BACK : I\'ll be back soon !           |'),nl,           
+                write('|         2. BUY  : I want to buy some stuff !     |'),nl,
+                write('|         3. SELL : I want to sell some stuff !    |'),nl,
+                write('|--------------------------------------------------|'),nl,
 
-                write('|-----------------------------------------------|'),nl,
-                write('|          Welcome To The Marketplace !         |'),nl,
-                write('|-----------------------------------------------|'),nl,
-                write(' 1. BACK : I\'ll be back soon !'),nl,
-                write(' 2. BUY  : I want to buy some stuff !'),nl,
-                write(' 3. SELL : I want to sell some stuff !'),nl,
-
-                write('What do you want to do ?'),nl,
                 read(Choice),nl,
 
-                (Choice = BACK ->
+                (Choice = back ->
                     write('Okay, make sure to come back later !')
                     ;
-                Choice = BUY ->
+                Choice = buy ->
                     write('|--------------------------------------------|'),nl,
                     write('|          What do you want to buy ?         |'),nl,
                     write('|--------------------------------------------|'),nl,
-                    /*Display item to buy*/
+                    write('|         1. Carrot seed (50 golds)          |'),nl,
+                    write('|         2. Corn seed (50 golds)            |'),nl,
+                    write('|         3. Tomato seed (50 golds)          |'),nl,
+                    write('|         4. Potato seed (50 golds)          |'),nl,
+                    write('|         5. Chicken (500 golds)             |'),nl,
+                    write('|         6. Sheep (1000 golds)              |'),nl,
+                    write('|         7. Cow (1500 golds)                |'),nl,
+                    write('|         8. Level 2 Shovel                  |'),nl,
+                    write('|         9. Level 3 Fishing rod             |'),nl,
+                    write('|--------------------------------------------|'),nl,
+
+                    read(Choicebuy),nl,
                     ;
-                Choice = SELL ->
+                    
+                Choice = sell ->
                     write('|--------------------------------------------|'),nl,
                     write('|          What do you want to sell ?        |'),nl,
                     write('|--------------------------------------------|'),nl,
