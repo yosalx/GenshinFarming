e7b993bf09c152fc5c6c25b3a956ece6d0b68fd0
jakartasipirok <kevinroni2002@gmail.com>
2021-11-29 01:37:53 +0700

    update house

--- house.pl
+++ house.pl
@@ -1,5 +1,8 @@
-:- dynamic(time/1).
-time(365).
+:- include('explore.pl').
+:- dynamic(day/1).
+:- include('map.pl').
+:- include('peta.pl').
+day(364).
 
 house :-
     write('What do you want to do'),nl,
@@ -9,12 +12,12 @@ house :-
     write('- exit'),nl.
 
 sleep :-
-    time(N),
+    day(N),
     (N < 365 ->
         write('You went to sleep'),nl,nl,
         NextN is N+1,
-        retract(time(N)),
-        assertz(time(NextN)),
+        retract(day(N)),
+        assertz(day(NextN)),
         write('Day '), write(NextN)
         ;
     N = 365 ->
@@ -24,4 +27,67 @@ sleep :-
 failState :-
     write('You fail bro').
 
+peritidur :-
+    random(1,15,X),
+    (X = 13 ->
+        write('hi im peri tidur. Where do you want to go?'),
+        write('1. Ranch'),nl,
+        write('2. Pond'),nl,
+        write('3. Marketplace'),nl,
+        read(Choice),
+        (Choice = 1 ->
+            lokasi(ranch,X,Y),
+            teleport(X,Y),
+            retract(atRanch(0)),
+            assertz(atRanch(1)),
+            write('masuk ranch')
+            ;
+        Choice = 2 ->
+            teleport(4,8),
+            write('hahy')
+            ;
+        Choice = 3 ->
+            lokasi(market,X,Y),
+            teleport(X,Y),
+            retract(atMarketplace(0)),
+            assertz(atMarketplace(1)),
+            write('masuk market')
+            ;
+        write('defal')
+        )
+    ;
+    write('gagal'),
+    write('')
+    ).
 
+:- dynamic(listDiary/1).
+listDiary([]).
+
+writeDiary :-
+    listDiary(List),
+    write('write your diary here'),nl,
+    read(Input),
+    day(Day),
+    append(List, [[Day, Input]], NewList),
+    retract(listDiary(List)),
+    assertz(listDiary(NewList)),
+    write('done').
+
+showDiaryEntry([]) :-
+    write('').
+
+showDiaryEntry([[Day, _]|T]) :-
+    write('Day '), write(Day), nl,
+    showDiaryEntry(T).
+
+
+readDiary :-
+    listDiary(List),
+    showDiaryEntry(List),
+    write('select day'),nl,
+    read(Choice),
+    (member([Choice, Entry], List) ->
+        write(Entry)
+        ;
+    write('Day tidak ada')
+    ).
