53dfdc290d751cd92e58f7992e620f2a7fa5a993
Jundan Haris <jundanha@gmail.com>
2021-11-28 17:57:00 +0700

    add gacha (not finished yet)

--- fishing.pl
+++ fishing.pl
@@ -7,7 +7,7 @@ fish :-
 
 
 fish :- 
-    equipment('fishing Rod', _, 1),
+    equipment('fishing Rod', Level, 1),
     lokasi(player, X, Y),
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     W is Y-1,
@@ -18,7 +18,7 @@ fish :-
     */
 
 fish :- 
-    equipment('fishing Rod', _, 1),
+    equipment('fishing Rod', Level, 1),
     lokasi(player, X, Y),
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     A is X-1,
@@ -29,7 +29,7 @@ fish :-
     */
 
 fish :- 
-    equipment('fishing Rod', _, 1),
+    equipment('fishing Rod', Level, 1),
     lokasi(player, X, Y),
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     S is Y+1,
@@ -40,7 +40,7 @@ fish :-
     */
 
 fish :- 
-    equipment('fishing Rod', _, 1),
+    equipment('fishing Rod', Level, 1),
     lokasi(player, X, Y),
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     D is X+1,
@@ -82,5 +82,92 @@ fish :-
 fish :-
     write('|----------------------------------------------------|'),nl,
     write('|      Can\'t fish, stand near water first! ^_^       |'),nl,
+    write('|----------------------------------------------------|'),nl
+    .
+
+
+gacha(LevelFishing) :-
+    subgacha(LevelFishing).
+
+subgacha(1) :-
+    random(1, 101, R),
+    subsubgacha1(R, Hasil).
+    printHasil(Hasil).
+
+subgacha(2) :-
+    random(1, 101, R),
+    subsubgacha2(R, Hasil),
+    printHasil(Hasil).
+
+subgacha(3) :-
+    random(1, 101, R),
+    subsubgacha2(3, Hasil),
+    printHasil(Hasil).
+
+/* hasil gacha buat level 1 exp fishing*/
+subsubgacha1(R, Hasil) :-
+    R == 1, 
+    Hasil is Tuna.
+subsubgacha1(R, Hasil) :-
+    R>1, R<7, 
+    Hasil is Salmon.
+subsubgacha1(R, Hasil) :-
+    R>6, R<21, 
+    Hasil is Gurame.
+subsubgacha1(R, Hasil) :-
+    R>20, R<41, 
+    Hasil is Tongkol.
+subsubgacha1(R, Hasil) :-
+    R>40, R<71, 
+    Hasil is Lele.
+subsubgacha1(R, Hasil) :-
+    R>70, R<101, 
+    Hasil is Garbage.
+
+
+printHasil(Hasil) :-
+    Hasil == Tuna,
+    addItem('Tuna', 1),
+    write('|----------------------------------------------------|'),nl,
+    write('|      Congratulations! You got Tuna (Rank S)        |'),nl,
+    write('|----------------------------------------------------|'),nl
+    .
+
+printHasil(Hasil) :-
+    Hasil == Salmon,
+    addItem('Salmon', 1),
+    write('|----------------------------------------------------|'),nl,
+    write('|      Congratulations! You got Salmon (Rank A)      |'),nl,
+    write('|----------------------------------------------------|'),nl
+    .
+
+printHasil(Hasil) :-
+    Hasil == Gurame,
+    addItem('Gurame', 1),
+    write('|----------------------------------------------------|'),nl,
+    write('|      Congratulations! You got Gurame (Rank B)      |'),nl,
+    write('|----------------------------------------------------|'),nl
+    .
+
+printHasil(Hasil) :-
+    Hasil == Tongkol,
+    addItem('Tongkol', 1),
+    write('|----------------------------------------------------|'),nl,
+    write('|      Congratulations! You got Tongkol (Rank C)     |'),nl,
+    write('|----------------------------------------------------|'),nl
+    .
+
+printHasil(Hasil) :-
+    Hasil == Lele,
+    addItem('Lele', 1),
+    write('|----------------------------------------------------|'),nl,
+    write('|      Congratulations! You got Lele (Rank D)        |'),nl,
+    write('|----------------------------------------------------|'),nl
+    .
+
+printHasil(Hasil) :-
+    Hasil == Garbage,
+    write('|----------------------------------------------------|'),nl,
+    write('|      Sorry, You got nothing. Keep fishing! ^_^     |'),nl,
     write('|----------------------------------------------------|'),nl
     .
