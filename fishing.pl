6f974fa6b90781874f7a3eed936d2783fc6a9185
Jundan Haris <jundanha@gmail.com>
2021-11-28 22:25:22 +0700

    fixing gacha and delete comment

--- fishing.pl
+++ fishing.pl
@@ -14,11 +14,7 @@ fish :-
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     W is Y-1,
     lokasi(air, X, W), !,
-    gacha(1, A),
-    write('sukses mancing w'), nl.
-    /*
-    gacha disini
-    */
+    gacha(1, A).
 
 fish :- 
     equipment('Fishing Rod', A, 1),
@@ -26,11 +22,7 @@ fish :-
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     A is X-1,
     lokasi(air, A, Y), !,
-    gacha(1, A),
-    write('sukses mancing a'), nl.
-    /*
-    gacha disini
-    */
+    gacha(1, A).
 
 fish :- 
     equipment('Fishing Rod', A, 1),
@@ -38,11 +30,7 @@ fish :-
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     S is Y+1,
     lokasi(air, X, S), !,
-    gacha(1, A),
-    write('sukses mancing s'), nl.
-    /*
-    gacha disini
-    */
+    gacha(1, A).
 
 fish :- 
     equipment('Fishing Rod', A, 1),
@@ -50,11 +38,7 @@ fish :-
     atRanch(0), atMarketplace(0), atQuest(0), atHouse(0),
     D is X+1,
     lokasi(air, D, Y), !,
-    gacha(1, A),
-    write('sukses mancing d'), nl.
-    /*
-    gacha disini
-    */
+    gacha(1, A).
 
 
 fish :-
@@ -97,165 +81,60 @@ gacha(LevelFishing, LevelFishingRod) :-
 
 subgacha(1, 1) :-
     random(1, 101, R11),
-    (R11 = 1 -> printHasil(Tuna); (R11>1, R11<7) -> printHasil(Salmon); (R11 > 6, R11<21) -> printHasil(Gurame); (R11>20, R11<41) -> printHasil(Tongkol); (R11>40, R11<71) -> printHasil(Lele); (R11>70, R11<101) -> printHasil(Garbage)).
-
-/*
-    subsubgacha1(R11, Hasil11).
-    printHasil(Hasil11).
+    (R11 = 1 -> printHasil(tuna); 
+    (R11>1, R11<7) -> printHasil(salmon); 
+    (R11 > 6, R11<21) -> printHasil(gurame); 
+    (R11>20, R11<41) -> printHasil(tongkol); 
+    (R11>40, R11<71) -> printHasil(lele); 
+    (R11>70, R11<101) -> printHasil(garbage)).
 
 subgacha(2, 1) :-
     random(1, 101, R21),
-    subsubgacha2(R21, Hasil21),
-    printHasil(Hasil21).
+    ((R21>0, R21<6) -> printHasil(tuna); 
+    (R21>5, R21<16) -> printHasil(salmon); 
+    (R21>15, R21<31) -> printHasil(gurame); 
+    (R21>30, R21<61) -> printHasil(tongkol); 
+    (R21>60, R21<81) -> printHasil(lele); 
+    (R21>80, R21<101) -> printHasil(garbage)).
 
 subgacha(3, 1) :-
     random(1, 101, R31),
-    subsubgacha3(R31, Hasil31),
-    printHasil(Hasil31).
+    ((R31>0, R31<11) -> printHasil(tuna); 
+    (R31>10, R31<24) -> printHasil(salmon); 
+    (R31>23, R31<44) -> printHasil(gurame); 
+    (R31>43, R31<79) -> printHasil(tongkol); 
+    (R31>78, R31<91) -> printHasil(lele); 
+    (R31>90, R31<101) -> printHasil(garbage)).
 
 subgacha(1, 2) :-
     random(1, 101, R12),
-    subsubgacha4(R12, Hasil12).
-    printHasil(Hasil12).
+    ((R12>0, R12<3) -> printHasil(tuna); 
+    (R12>2, R12<9) -> printHasil(salmon); 
+    (R12>8, R12<21) -> printHasil(gurame); 
+    (R12>20, R12<41) -> printHasil(tongkol); 
+    (R12>40, R12<71) -> printHasil(lele); 
+    (R12>70, R12<101) -> printHasil(garbage)).
 
 subgacha(2, 2) :-
     random(1, 101, R22),
-    subsubgacha5(R22, Hasil22),
-    printHasil(Hasil22).
+    ((R22>0, R22<8) -> printHasil(tuna); 
+    (R22>7, R22<20) -> printHasil(salmon); 
+    (R22>19, R22<36) -> printHasil(gurame); 
+    (R22>35, R22<71) -> printHasil(tongkol); 
+    (R22>70, R22<86) -> printHasil(lele); 
+    (R22>85, R22<101) -> printHasil(garbage)).
 
 subgacha(3, 2) :-
     random(1, 101, R32),
-    subsubgacha6(R32, Hasil32),
-    printHasil(Hasil32).
-
-/* hasil gacha buat level 1 exp fishing dengan fishing rod level 1 */ 
-/*
-subsubgacha1(R, Hasil) :-
-    R is 1, 
-    Hasil is Tuna,!.
-subsubgacha1(R, Hasil) :-
-    R>1, R<7, 
-    Hasil is Salmon,!.
-subsubgacha1(R, Hasil) :-
-    R>6, R<21, 
-    Hasil is Gurame,!.
-subsubgacha1(R, Hasil) :-
-    R>20, R<41, 
-    Hasil is Tongkol,!.
-subsubgacha1(R, Hasil) :-
-    R>40, R<71, 
-    Hasil is Lele,!.
-subsubgacha1(R, Hasil) :-
-    R>70, R<101, 
-    Hasil is Garbage,!.
-
-/* hasil gacha buat level 2 exp fishing dengan fishing rod level 1 */
-/*
-subsubgacha2(R, Hasil) :-
-    R > 0, R < 6,
-    Hasil is Tuna,!.
-subsubgacha2(R, Hasil) :-
-    R>5, R<16, 
-    Hasil is Salmon,!.
-subsubgacha2(R, Hasil) :-
-    R>15, R<31, 
-    Hasil is Gurame,!.
-subsubgacha2(R, Hasil) :-
-    R>30, R<61, 
-    Hasil is Tongkol,!.
-subsubgacha2(R, Hasil) :-
-    R>60, R<81, 
-    Hasil is Lele,!.
-subsubgacha2(R, Hasil) :-
-    R>80, R<101, 
-    Hasil is Garbage,!.
-
-/* hasil gacha buat level 3 exp fishing dengan fishing rod level 1 */
-/*
-subsubgacha3(R, Hasil) :-
-    R>0, R<11, 
-    Hasil is Tuna,!.
-subsubgacha3(R, Hasil) :-
-    R>10, R<24, 
-    Hasil is Salmon,!.
-subsubgacha3(R, Hasil) :-
-    R>23, R<44, 
-    Hasil is Gurame,!.
-subsubgacha3(R, Hasil) :-
-    R>43, R<79, 
-    Hasil is Tongkol,!.
-subsubgacha3(R, Hasil) :-
-    R>78, R<91, 
-    Hasil is Lele,!.
-subsubgacha3(R, Hasil) :-
-    R>90, R<101, 
-    Hasil is Garbage,!.
-
-/* hasil gacha buat level 1 exp fishing dengan fishing rod level 2 */
-/*
-subsubgacha4(R, Hasil) :-
-    R>1, R<3, 
-    Hasil is Tuna,!.
-subsubgacha4(R, Hasil) :-
-    R>2, R<9, 
-    Hasil is Salmon, !.
-subsubgacha4(R, Hasil) :-
-    R>8, R<21, 
-    Hasil is Gurame, !.
-subsubgacha4(R, Hasil) :-
-    R>20, R<41, 
-    Hasil is Tongkol, !.
-subsubgacha4(R, Hasil) :-
-    R>40, R<71, 
-    Hasil is Lele,!.
-subsubgacha4(R, Hasil) :-
-    R>70, R<101, 
-    Hasil is Garbage, !.
-
-/* hasil gacha buat level 2 exp fishing dengan fishing rod level 2 */
-/*
-subsubgacha5(R, Hasil) :-
-    R>0, R<8,
-    Hasil is Tuna,!.
-subsubgacha5(R, Hasil) :-
-    R>7, R<20, 
-    Hasil is Salmon, !.
-subsubgacha5(R, Hasil) :-
-    R>19, R<36, 
-    Hasil is Gurame, !.
-subsubgacha5(R, Hasil) :-
-    R>35, R<71, 
-    Hasil is Tongkol, !.
-subsubgacha5(R, Hasil) :-
-    R>70, R<86, 
-    Hasil is Lele, !.
-subsubgacha5(R, Hasil) :-
-    R>85, R<101, 
-    Hasil is Garbage, !.
-
-/* hasil gacha buat level 3 exp fishing dengan fishing rod level 2 */
-/*
-subsubgacha6(R, Hasil) :-
-    R>0, R<13,
-    Hasil is Tuna, !.
-subsubgacha6(R, Hasil) :-
-    R>12, R<28, 
-    Hasil is Salmon, !.
-subsubgacha6(R, Hasil) :-
-    R>27, R<53, 
-    Hasil is Gurame, !.
-subsubgacha6(R, Hasil) :-
-    R>52, R<86, 
-    Hasil is Tongkol, !.
-subsubgacha6(R, Hasil) :-
-    R>85, R<96, 
-    Hasil is Lele, !.
-subsubgacha6(R, Hasil) :-
-    R>95, R<101, 
-    Hasil is Garbage, !. */
+    ((R32>0, R32<13) -> printHasil(tuna); 
+    (R32>12, R32<28) -> printHasil(salmon); 
+    (R32>27, R32<53) -> printHasil(gurame); 
+    (R32>52, R32<86) -> printHasil(tongkol); 
+    (R32>85, R32<96) -> printHasil(lele); 
+    (R32>95, R32<101) -> printHasil(garbage)).
 
 printHasil(Hasil) :-
-    Hasil is Tuna,
+    Hasil = tuna ->
     addItem('Tuna', 1),
     write('|----------------------------------------------------|'),nl,
     write('|      Congratulations! You got Tuna (Rank S)        |'),nl,
@@ -263,7 +142,7 @@ printHasil(Hasil) :-
     .
 
 printHasil(Hasil) :-
-    Hasil is Salmon,
+    Hasil = salmon ->
     addItem('Salmon', 1),
     write('|----------------------------------------------------|'),nl,
     write('|      Congratulations! You got Salmon (Rank A)      |'),nl,
@@ -271,7 +150,7 @@ printHasil(Hasil) :-
     .
 
 printHasil(Hasil) :-
-    Hasil is Gurame,
+    Hasil = gurame ->
     addItem('Gurame', 1),
     write('|----------------------------------------------------|'),nl,
     write('|      Congratulations! You got Gurame (Rank B)      |'),nl,
@@ -279,7 +158,7 @@ printHasil(Hasil) :-
     .
 
 printHasil(Hasil) :-
-    Hasil is Tongkol,
+    Hasil = tongkol ->
     addItem('Tongkol', 1),
     write('|----------------------------------------------------|'),nl,
     write('|      Congratulations! You got Tongkol (Rank C)     |'),nl,
@@ -287,7 +166,7 @@ printHasil(Hasil) :-
     .
 
 printHasil(Hasil) :-
-    Hasil is Lele,
+    Hasil = lele ->
     addItem('Lele', 1),
     write('|----------------------------------------------------|'),nl,
     write('|      Congratulations! You got Lele (Rank D)        |'),nl,
@@ -295,7 +174,7 @@ printHasil(Hasil) :-
     .
 
 printHasil(Hasil) :-
-    Hasil is Garbage,
+    Hasil = garbage ->
     write('|----------------------------------------------------|'),nl,
     write('|      Sorry, You got nothing. Keep fishing! ^_^     |'),nl,
     write('|----------------------------------------------------|'),nl
